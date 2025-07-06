e2270f2a9fd45d07919946cec7f120f95a552ee8
Rajkumar <rajesram999@gmail.com>
2025-07-06 23:09:30 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 export default function Hero() {
   return (
-     <h2 className="text-3xl sm:text-4xl font-bold text-center bg-gradient-to-r from-blue-600 to-purple-600 text-white py-3 px-4 rounded-md shadow-lg uppercase tracking-wide mb-6">
-  Home
-</h2>
+     <section id="home" className="py-20 px-4 text-center">
       <h1 className="text-4xl font-bold bg-green-400 text-white p-2 rounded-md">
   Hi, I'm Rajkumar Esram
 </h1>
