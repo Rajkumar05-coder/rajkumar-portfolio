fe4e19ddc76d688ba7c581f0327eb7eca56b7ebe
Rajkumar <rajesram999@gmail.com>
2025-07-06 23:06:25 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,10 @@ export default function About() {
   return (
     <section id="about" className="py-20 px-4 bg-white dark:bg-gray-900 text-center">
       <div className="max-w-3xl mx-auto">
-        <h2 className="text-3xl font-bold mb-6 text-gray-800 dark:text-white">About Me</h2>
+    <h2 className="text-3xl sm:text-4xl font-bold text-center bg-gradient-to-r from-green-600 to-emerald-500 text-white py-3 px-4 rounded-md shadow-lg uppercase tracking-wide mb-6">
+  About
+</h2>
+
         <p className="text-gray-700 dark:text-gray-300 mb-4">
           I'm <strong>Rajkumar Esram</strong>, a passionate developer skilled in Python, Data Analysis, and Web Development.
         </p>
