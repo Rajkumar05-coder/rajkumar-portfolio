13cde2e8a409a191ae8d802a90a8958c4d7cb80d
Rajkumar <rajesram999@gmail.com>
2025-07-06 22:54:38 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,9 @@ export default function Hero() {
   return (
     <section id="home" className="py-20 px-4 text-center">
       <h1 className="text-4xl font-bold">Hi, I'm Rajkumar Esram</h1>
+      <h1 className="text-4xl font-bold bg-blue-500 text-white p-2 rounded-md">
+  Hi, I'm Rajkumar Esram
+</h1>
       <p className="mt-4 text-xl text-gray-600">Python Developer | Data Analyst | Web Developer</p>
     </section>
   );
