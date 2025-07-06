5de2b1cea4d545288b23fed15c039c7be6516724
Rajkumar <rajesram999@gmail.com>
2025-07-06 23:05:26 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,10 @@ export default function Hero() {
       <h1 className="text-4xl font-bold bg-green-400 text-white p-2 rounded-md">
   Hi, I'm Rajkumar Esram
 </h1>
+      <h2 className="text-3xl sm:text-4xl font-bold text-center bg-gradient-to-r from-blue-600 to-purple-600 text-white py-3 px-4 rounded-md shadow-lg uppercase tracking-wide mb-6">
+  Home
+</h2>
+
       <p className="mt-4 text-xl text-gray-600">Python Developer | Data Analyst | Web Developer</p>
     </section>
   );
